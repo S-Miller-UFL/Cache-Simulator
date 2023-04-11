9025dab799e3688906633ef97fedc86508a59a88
steven miller <sjmiller0403@gmail.com>
2023-04-11 11:09:54 -0400

    added all cache types

--- cache_simulator/cache_map.h
+++ cache_simulator/cache_map.h
@@ -1,35 +1,179 @@
 #pragma once
 #include <map>
 #include <iostream>
-class cache
+#include <string>
+#include <math.h>
+#include <vector>
+class direct_cache
 {
 public:
-	struct line
-	{
-		int tag = 0;
-		int counter = 0;
-	};
-	//init cache-> takes in size of cache and an optional associativity number
-	void print_cache();
-	void init_cache(int,int);
+	void populate(int, int,std::string);
+	void initialize_cache(int);
+	void print();
 private:
-	//map->index(set number),line
-	std::map<int, line*> cache;
+	int hit = 0;
+	int miss = 0;
+	//std::map<int,block*> cache;
+	std::vector <std::tuple<long long, long long , long long, char >> cache;
 };
-
-void cache::init_cache(int size, int associativity)
+void direct_cache::initialize_cache(int numberoflines)
 {
-	for (int i = 0; i < size/32; i++)
+	for (int i = 0; i < numberoflines; i++)
 	{
-		line* l = new line;
-		l->counter = 0x00000000;
-		cache.emplace(i,l);
+		cache.push_back(std::make_tuple(0, 0, 0, 'E'));
 	}
 }
-void cache::print_cache()
+void direct_cache::populate(int bytesinline,int numberoflines,std::string address)
+{
+	int offsetwidth = ceil(log2(bytesinline));
+	int linewidth = log2(numberoflines);
+	int tagwidth = address.size() - offsetwidth - linewidth;
+	std::string tag = address.substr(0,tagwidth);
+	std::string offset = address.substr(tagwidth+linewidth,offsetwidth);
+	std::string line = address.substr(tagwidth, linewidth);
+	std::cout << "tag: " << tag <<std::endl;
+	std::cout << "offset: " << offset << std::endl;
+	std::cout << "line: " << line << std::endl;
+	cache.at(std::stoll(line, nullptr, 2)) = (std::make_tuple(std::stoll(tag,nullptr,2), std::stoll(line,nullptr,2), std::stoll(offset,nullptr,2), 'F'));
+
+}
+void direct_cache::print()
 {
 	for (int i = 0; i < cache.size(); i++)
 	{
-		std::cout << i << " " << cache.at(i)->tag << std::endl;
+		std::cout << std::get<0>(cache.at(i)) << std::endl;
 	}
+}
+
+class set_cache
+{
+public:
+	void populate(int, int,int, std::string);
+	void initialize_cache(int);
+	void print();
+private:
+	int hit = 0;
+	int miss = 0;
+	std::vector<std::tuple<int, int, int, char>> cache;
+
+};
+void set_cache::initialize_cache(int numberoflines)
+{
+	for (int i = 0; i < numberoflines; i++)
+	{
+		cache.push_back(std::make_tuple(0, 0, 0, 'E'));
+	}
+}
+void set_cache::populate(int bytesinline, int numberoflines,int linesinset, std::string address)
+{
+	int offsetwidth = ceil(log2(bytesinline));
+	int setwidth = ceil(log2(numberoflines/linesinset));
+	int tagwidth = address.size() - offsetwidth - setwidth;
+	std::string tag = address.substr(0, tagwidth);
+	std::string offset = address.substr(tagwidth + setwidth, offsetwidth);
+	std::string set = address.substr(tagwidth, setwidth);
+	std::cout << "tag: " << tag << std::endl;
+	std::cout << "offset: " << offset << std::endl;
+	std::cout << "set: " << set << std::endl;
+	int index = std::stoll(set, nullptr, 2) * linesinset; //first index of our set
+
+		for (int i = 0; i < linesinset; i++)
+		{
+			if (std::get<0>(cache.at(index)) == std::stoll(tag, nullptr, 2) && std::get<3>(cache.at(index)) == 'F')
+			{
+				std::cout << "hit" << std::endl;
+				hit++;
+				break;
+			}
+			else if (std::get<3>(cache.at(index + i)) == 'E')
+			{
+				cache.at(index + i) = (std::make_tuple(std::stoll(tag, nullptr, 2), std::stoll(set, nullptr, 2), std::stoll(offset, nullptr, 2), 'F'));
+				break;
+			}
+		}
+		miss++;
+		std::cout << "miss" << std::endl;
+	
+	/*
+	* 0011 1111 1111 1111 1111 1000 00 000000
+	* 0011 1111 1111 1111 1111 1000 00 000000
+	* 0011 1111 1111 1111 1111 1000 01 000000
+	* 0011 1111 1111 1111 1111 1000 01 000000
+	* 0011 1111 1111 1111 1111 1000 10 000000
+	* 0011 1111 1111 1111 1111 1000 10 000000
+	* 0011 1111 1111 1111 1111 1000 11 000000
+	* 0011 1111 1111 1111 1111 1000 11 000000
+	*/
+
+}
+void set_cache::print()
+{
+	for (int i = 0; i < cache.size(); i++)
+	{
+		std::cout << "set: " << std::get<1>(cache.at(i))<< " tag: " << std::get<0>(cache.at(i)) << " offset: "<< std::get<2>(cache.at(i)) << std::endl;
+	}
+}
+class fully_cache
+{
+public:
+	void populate(int, int, std::string);
+	void initialize_cache(int);
+	void print();
+private:
+	int hit = 0;
+	int miss = 0;
+	std::vector<std::tuple<long long, long long, char>> cache;
+
+};
+void fully_cache::initialize_cache(int numberoflines)
+{
+	for (int i = 0; i < numberoflines; i++)
+	{
+		cache.push_back(std::make_tuple(0, 0, 'E'));
+	}
+}
+void fully_cache::populate(int bytesinline, int numberoflines, std::string address)
+{
+
+	int offsetwidth = ceil(log2(bytesinline));
+	int linewidth = 0;
+	int tagwidth = address.size() - offsetwidth - linewidth;
+	std::string tag = address.substr(0, tagwidth);
+	std::string offset = address.substr(tagwidth + linewidth, offsetwidth);
+	//std::string line = address.substr(tagwidth, linewidth);
+	std::cout << "tag: " << tag << std::endl;
+	std::cout << "offset: " << offset << std::endl;
+	//std::cout << "line: " << line << std::endl;
+	for (int i = 0; i < cache.size(); i++)
+	{
+		
+		if ((std::get<2>(cache.at(i))) == 'F' && (std::get<0>(cache.at(i)) == std::stoll(tag, nullptr, 2)))
+		{
+			hit++;
+			break;
+		}
+		else if (std::get<2>(cache.at(i)) == 'E')
+		{
+			cache.at(i) = (std::make_tuple(std::stoll(tag, nullptr, 2), std::stoll(offset, nullptr, 2), 'F'));
+			miss++;
+			break;
+		}
+	}
+	
+
+}
+void fully_cache::print()
+{
+	for (int i = 0; i < cache.size(); i++)
+	{
+		std::cout << std::get<0>(cache.at(i)) << std::endl;
+	}
+}
+long binarystringtolong(std::string str)
+{
+	for (int i = str.size(); i > 0; i--)
+	{
+
+	}
+	return 0;
 }
--- cache_simulator/cache_simulator.cpp
+++ cache_simulator/cache_simulator.cpp
@@ -1,6 +1,13 @@
 
 #include <iostream>
 #include "cache_map.h"
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <math.h>
+#include <bitset>
 //1.create 3 map-based caches
 //  1.fully associative
 //  2.direct mapped
@@ -19,6 +26,9 @@ int main()
     int selection = 0;
     int cache_type = 0;
     int size = 0;
+    int linesize = 0;
+    int hit = 0;
+    int miss = 0;
     std::cout << "input the type of cache to be implemented:" << std::endl;
     std::cout << "type 1 for fully associative" << std::endl;
     std::cout << "type 2 for direct mapped" << std::endl;
@@ -39,10 +49,120 @@ int main()
 
     std::cout << "how big (in bytes) will the cache be?" << std::endl;
     std::cin >> size;
+    std::cout << "how big will a block be?" << std::endl;
+    std::cin >> linesize;
+    char byte;
+    short count;
+    std::string line;
+    std::vector<std::string> v;
+    std::ifstream file("read01.trace",std::ios_base::binary);
+    if (file.is_open())
+    {
+        while (!file.eof())
+        {
+            file.read(&byte, 1);
+            file.read(&byte, 1);
+            //file.read(&byte, 1);
+            //file.read(&byte, 1);
+            for (int i = 0; i < 11; i++)
+            {
+                file.read(&byte, 1);
+                if (byte != '\n')
+                {
+                    line = line + byte;
+                }
+                //line = line + byte;
+                
+            }
+            file.read(&byte, 1);
+            file.read(&byte, 1);
+            if (line.size() != 0)
+            {
+                v.push_back(line);
+            }
+            line = "";
 
-    cache c;
-    c.init_cache(size, 0);
-    c.print_cache();
+        }
+        file.close();
+    }
+    else
+    {
+        std::cout << "cannot find file" << std::endl;
+    }
+    for (int i = 0; i < v.size(); i++)
+    {
+        int val = std::stol(v.at(i), nullptr, 16);
+        v.at(i) = std::bitset<32>(val).to_string();
+        //std::cout << v.at(i) << std::endl;
+    }
+    if (selection == 1)
+    {
 
 
+        //directed map
+        /*
+        * split into appropriate fields
+        - if Direct Mapped then: Tag/Line/Offset
+        - if Set Associative then: Tag/Set/Offset
+        - if Fully Associative then: Tag/Offset
+        */
+
+        /*
+        * 1.convert to binary
+        */
+        //2.split tag line offset
+
+        //3.store in data structure
+        direct_cache dc;
+        dc.initialize_cache(size / linesize);
+        for (int i = 0; i < v.size(); i++)
+        {
+            dc.populate(linesize, size / linesize, v.at(i));
+        }
+        //dc.populate(linesize, size / linesize, v.at(0));
+        dc.print();
+    }
+    else if (selection == 2)
+    {
+        /*
+        * full associative
+        */
+        fully_cache fc;
+        //1.convert to binary
+
+        //2. split tag/offset
+        fc.initialize_cache(size / linesize);
+        for (int i = 0; i < v.size(); i++)
+        {
+            fc.populate(linesize, size / linesize, v.at(i));
+        }
+        fc.print();
+    }
+    else if (selection == 3)
+    {
+        /*
+        * set associative map
+        */
+        //1.convert to binary
+        //2.split tag/set/offset
+        int linesinset = 0;
+        std::cout << "how many sets per line?" << std::endl;
+        std::cin >> linesinset;
+        set_cache sc;
+        sc.initialize_cache(size / linesize);
+        for (int i = 0; i < v.size(); i++)
+        {
+            if (sc.populate(linesize, size / linesize, linesinset, v.at(i))==true)
+            {
+                hit++;
+            }
+            else
+            {
+                miss++;
+            }
+        }
+        sc.print();
+    }
+    std::cout << "hit: " << hit << std::endl;
+    std::cout << "miss: " << miss << std::endl;
 }
